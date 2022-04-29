93240a98a75d90f452230b5bef0e2276dc931774
yanis khellaf <yanis.khellaf@outlook.fr>
2022-04-29 04:36:50 +0200

    blog and photo models

--- Fotoblog/urls.py
+++ Fotoblog/urls.py
@@ -16,15 +16,13 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(
         template_name='authentication/logged_out.html'),
         name='logout'),
-    # path('password_change/', PasswordChangeView.as_view(
-    #     template_name = 'authentication/password_change_form.html',
-    #     success_url = 'password_change_done',
-    #     ), 
-    #     name='password_change'),
-    # path('password_change/password_change_done/', PasswordChangeDoneView.as_view(
-    #     template_name = 'authentication/password_change_done.html'
-    #     ),
-    #     name='change_success'),
-    # path('profile-photo/upload', authentication.views.UploadProfilePhotoView.as_view(),
-    #     name='upload_profile_photo'),
+    path('password_change/', PasswordChangeView.as_view(
+        template_name = 'authentication/password_change_form.html',
+        success_url = 'password_change_done',
+        ), 
+        name='password_change'),
+    path('password_change/password_change_done/', PasswordChangeDoneView.as_view(
+        template_name = 'authentication/password_change_done.html'
+        ),
+        name='change_success'),
 ]
