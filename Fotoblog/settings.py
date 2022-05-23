b23e2ed71c8ee9eb6f47e04a86954ddc7bb2a886
yanis khellaf <yanis.khellaf@outlook.fr>
2022-05-23 17:21:26 +0200

    correction erreur serveur 500

--- Fotoblog/settings.py
+++ Fotoblog/settings.py
@@ -159,7 +159,7 @@ if os.environ.get('env') == 'PRODUCTION':
 
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' 
+    
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
