ecc153b0e4512c3177584e94ed8f237030bf755a
yanis khellaf <yanis.khellaf@outlook.fr>
2022-05-24 17:43:06 +0200

    correction placeholder

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,8 +4,10 @@ from django.contrib.auth import get_user_model
 # from django.forms.widgets import PasswordInput, TextInput
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label="Nom d'utilisateur", widget=forms.TextInput(attrs={'placeholder': 'Démo'}))
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput(attrs={'placeholder': 'Démo'}), label="Mot de passe")
+    # username = forms.CharField(max_length=63, label="Nom d'utilisateur", widget=forms.TextInput(attrs={'placeholder': 'Démo'}))
+    # password = forms.CharField(max_length=63, label="Mot de passe", widget=forms.PasswordInput(attrs={'placeholder': 'Démoblog1'}))
+    username = forms.CharField(label='username',widget=forms.TextInput(attrs={'placeholder':'Démo'}))
+    password = forms.CharField(label='password',widget=forms.PasswordInput(attrs={'placeholder':'Démoblog1'}))
 
 
 class SignUpForm(UserCreationForm):
