46e03291d3d8884b8fa3708129ba509f882fae9a
yanis khellaf <yanis.khellaf@outlook.fr>
2022-06-14 16:55:11 +0200

    refonte front

--- authentication/forms.py
+++ authentication/forms.py
@@ -6,14 +6,24 @@ from django.contrib.auth import get_user_model
 class LoginForm(forms.Form):
     # username = forms.CharField(max_length=63, label="Nom d'utilisateur", widget=forms.TextInput(attrs={'placeholder': 'Démo'}))
     # password = forms.CharField(max_length=63, label="Mot de passe", widget=forms.PasswordInput(attrs={'placeholder': 'Démoblog1'}))
-    username = forms.CharField(label='username',widget=forms.TextInput(attrs={'placeholder':'Démo'}))
-    password = forms.CharField(label='password',widget=forms.PasswordInput(attrs={'placeholder':'Démoblog1'}))
+    username = forms.CharField(label='username',widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
 
 class SignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username', 'email', 'first_name', 'last_name', 'role')
+
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['first_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['role'].widget.attrs['class'] = 'form-control'
 
 
 
