ab87de60f2910a83a0aa27f678b5824fa0e69f73
yanis khellaf <yanis.khellaf@outlook.fr>
2022-06-14 17:21:25 +0200

    refonte front

--- authentication/forms.py
+++ authentication/forms.py
@@ -20,19 +20,19 @@ class SignUpForm(UserCreationForm):
         super(SignUpForm, self).__init__(*args, **kwargs)
         
         self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['username'].widget.attrs['id'] = "floatingInput"
+        # self.fields['username'].widget.attrs['id'] = "floatingInput"
 
         self.fields['email'].widget.attrs['class'] = 'form-control'
-        self.fields['email'].widget.attrs['id'] = "floatingInput"
+        # self.fields['email'].widget.attrs['id'] = "floatingInput"
 
         self.fields['first_name'].widget.attrs['class'] = 'form-control'
-        self.fields['first_name'].widget.attrs['id'] = "floatingInput"
+        # self.fields['first_name'].widget.attrs['id'] = "floatingInput"
 
         self.fields['last_name'].widget.attrs['class'] = 'form-control'
-        self.fields['last_name'].widget.attrs['id'] = "floatingInput"
+        # self.fields['last_name'].widget.attrs['id'] = "floatingInput"
 
         self.fields['role'].widget.attrs['class'] = 'form-control'
-        self.fields['role'].widget.attrs['id'] = "floatingInput"
+        # self.fields['role'].widget.attrs['id'] = "floatingInput"
 
 
 
